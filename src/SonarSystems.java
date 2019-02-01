43eefec49684faa1e954ca6984ecc7f952bd23a1
bxvail <BryantVail@gmail.com>
2019-02-01 02:42:02 -0500

    end print

--- src/SonarSystems.java
+++ src/SonarSystems.java
@@ -4,6 +4,9 @@ public class SonarSystems {
 	public static void main(String[] args) {
 		// 
 		System.out.println("Hello World");
+		System.out.println("print line");
+		System.out.println("print line 2");
+		System.out.println("print line 3");
 
 	}
 
